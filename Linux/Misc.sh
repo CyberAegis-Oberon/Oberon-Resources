93d840742a1e8750c715327f7e6063dc44c6724f
Soham360 <sohamkamat10@gmail.com>
2024-01-07 14:36:51 -0800

    j

--- Linux/Misc.sh
+++ Linux/Misc.sh
@@ -8,6 +8,15 @@ echo "PASS_MAX_DAYS	90" >> "/etc/login.defs" > /dev/null
 echo "PASS_MIN_DAYS	7" >> "/etc/login.defs" > /dev/null
 echo "PASS_MIN_LEN	14" >> "/etc/login.defs" > /dev/null
 echo "PASS_WARN_AGE	14" >> "/etc/login.defs" > /dev/null
+echo "PASS_MIN_DAYS	10" >> "/etc/login.defs" > /dev/null
+echo "FAILLOG_ENAB	yes" >> "/etc/login.defs" > /dev/null
+echo "LOG_UNKFAIL_ENAB	yes" >> "/etc/login.defs" > /dev/null
+echo "LOG_OK_LOGINS	yes" >> "/etc/login.defs" > /dev/null
+echo "SYSLOG_SU_ENAB	yes" >> "/etc/login.defs" > /dev/null
+echo "SYSLOG_SG_ENAB	yes" >> "/etc/login.defs" > /dev/null
+echo "LOGIN_RETRIES	5" >> "/etc/login.defs" > /dev/null
+echo "ENCRYPT_METHOD	SHA512" >> "/etc/login.defs" > /dev/null
+echo "LOGIN_TIMEOUT	60" >> "/etc/login.defs" > /dev/null
 
 # crontabs checking
 shopt -s nullglob dotglob     # To include hidden files
